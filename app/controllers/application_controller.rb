9019d7f5e3b68bc49416277ca00634a82732cc56
Crystal Perreira <cvperreira@gmail.com>
2015-01-28 11:52:45 -0800

    add current_user helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
   require './lib/flickr'
+
+  helper :all
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
 end
