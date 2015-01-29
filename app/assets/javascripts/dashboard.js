bec3a551210908230c83c1821041e00a4abdc719
Crystal Perreira <cvperreira@gmail.com>
2015-01-29 15:48:24 -0800

    check exif on submit, no submit if no exif

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,23 +1,9 @@
-/*jQuery(document).ready(function() {
-  $(".submit-photo").click(function(e) {
-    e.preventDefault();
-    alert("blah");
-  });
-});*/
 
 $(function () {
 
 
-  function is_exif_avail (photo_id) {
-    /*go to url*/
-    function process_exif_data (data) {
-      /*take out extra flickr stuff jsonFlickrApi( and )*/
-      var fixed = data.replace('jsonFlickrApi(', '');
-      fixed = fixed.substring(0, fixed.length-1);
-      var new_data = JSON.parse(fixed);
-      debugger
-      return new_data.stat === "ok"
-    }; 
+  function check_is_exif_avail (photo_id, onSuccess, onError) {
+
 
     $.ajax({
       url: ("https://api.flickr.com/services/rest/?&method=flickr.photos.getexif"
@@ -25,34 +11,50 @@ $(function () {
             + photo_id 
             + "&format=json"),
       success: process_exif_data,
-      dataType: "text",
-      error: function (e) {console.log(e); alert("there was error");}
+      dataType: "text", /*forced data type to be text else jquery prematurely interprets as json*/
+      error: function (e) {alert("There was error");}
     });
-    //get data
-    //return false if data is not there
-    //return data as params if it is there
+
   };
 
+  /*click handler*/
   $(".submit-photo").click(function (e) {
     e.preventDefault();
-    alert("blah");
     var photo_data = $(this).attr('info');
     var photo_id = JSON.parse(photo_data).id;
 
-    console.log(is_exif_avail(photo_id));
 
-    if (is_exif_avail(photo_id, postToTestOnSuccess, complainOnError)) {
-
+    function postToTestOnSuccess () {    
       $.ajax("/test", {
-          type: "POST",
-          data: {hash: photo_data},
-          success: function (data) {
-          }
+        type: "POST",
+        data: {hash: photo_data},
+        success: function (data) {
+        }
       });
-
     }
 
+    function complainOnError () {
+      alert("Please make sure Exif data is publicly available");
+    }
+
+    check_is_exif_avail(photo_id, postToTestOnSuccess, complainOnError);
   });
 
+
+
+  function process_exif_data (flickr_api_response) {
+    /*get rid of flickr junk*/
+    var fixed = flickr_api_response.replace('jsonFlickrApi(', '');
+    fixed = fixed.substring(0, fixed.length-1);
+
+    var api_response_json = JSON.parse(fixed);
+    
+    /*stat equals ok if exif avail otherwise equals fail*/
+    if (api_response_json.stat === "ok") {
+      onSuccess();
+    } else {
+      onError();
+    }
+  }; 
 
 });
