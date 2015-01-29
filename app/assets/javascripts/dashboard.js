fee095e5a95b028bcd87f348ca42b46b8e3c6787
Crystal Perreira <cvperreira@gmail.com>
2015-01-29 11:48:28 -0800

    js version of alert

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,13 +1,12 @@
-/*$(function () {
-
-
-  $.ajax(
-  );
-
-};*/
-
-jQuery(document).ready(function() {
+/*jQuery(document).ready(function() {
   $(".submit-photo").click(function(e) {
+    e.preventDefault();
+    alert("blah");
+  });
+});*/
+
+$(function () {
+  $(".submit-photo").click(function (e) {
     e.preventDefault();
     alert("blah");
   });
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,8 @@
 class PhotosController < ApplicationController
   require 'httparty'
 
+  def submit_photo
+    puts "blah" # will this show up in my server? 
+  end
 
 end
