7edc543878c22655f6fce43f49f4dc6cf0e895f8
Crystal Perreira <cvperreira@gmail.com>
2015-02-02 10:16:46 -0800

    start discrete values on slider

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,59 @@
 $(function () {
 
-  $('.ap-test').slider();
+
+
+
+var steps = [
+    "very sad",
+    "sad",
+    "not so sad",
+    "happy",
+    "very happy"
+];
+
+$(function() {
+    $(".ap-test").slider({
+        value: 1,
+        min: 0,
+        max: 4,
+        step: 1,
+        slide: function(event, ui) {
+            $("#hint").html(steps[ui.value]);
+        }
+    });
+    $("#hint").val(steps[$(".ap-test").slider("value")]);
+});
+
+
+
+
+
+
+
+/*var text = {
+    1: "bad",
+    2: "okay",
+    3: "better",
+    4: "great",
+    5: "awesome"
+};
+var $sliderText = $('#hint');
+
+var refresh = function (e) {
+    $sliderText.text(text[$('.ap-test').slider('value')]);
+};
+$(function () {
+    $('.ap-test').slider({
+        min: 1,
+        max: 5,
+        slide: refresh,
+        change: refresh
+    });
+    refresh();
+});*/
+
+
+
+
 
 });
