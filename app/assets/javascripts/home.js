232766a45631fdab0fe2a7052f8ca98180b8f604
Crystal Perreira <cvperreira@gmail.com>
2015-02-03 15:42:39 -0800

    clean up home.js

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
   function generate_slider (name_of_slider_divs, array_of_options, bound_attr, default_value_attr) {
     var steps = array_of_options
     var form_divs = $("."+name_of_slider_divs+"-slider")
+
     //loop over all of the sliders
      for(var i = 0; i<form_divs.length; i++){
       //give every slider an fid attr
@@ -12,7 +13,6 @@ $(document).ready(function () {
       var setting_used = $(form_divs[i]).attr(default_value_attr);
       var steps_as_floats = eval(steps);
       var steps_size = (steps.length) - 1
-      console.log("id:"+id+"bound:"+bound+"setting:"+setting_used+"size:"+steps_size);
       $("#"+id+"-"+name_of_slider_divs+"-slider").slider({
         value: find_nearest(steps_as_floats, eval(setting_used)),
         min: find_nearest(steps_as_floats, eval(bound)),
@@ -24,6 +24,7 @@ $(document).ready(function () {
         }
       }); 
       //debugging
+      //console.log("id:"+id+"bound:"+bound+"setting:"+setting_used+"size:"+steps_size);
       /*console.log(steps[
         $("#"+id+"-"+name_of_slider_divs+"-slider").slider("value")
       ]);
@@ -64,7 +65,6 @@ $(document).ready(function () {
     };
   };
 
-
   var aperture_steps = [
     "0.7", "0.8", "0.9", "1.0", "1.1", "1.2", "1.4", "1.6",
     "1.8", "2", "2.2", "2.5", "2.8", "3.2", "3.5", "4",
@@ -95,65 +95,4 @@ $(document).ready(function () {
 
 });
 
-
-
-  /*$(function() {
-
-    var steps = [
-      "0.7", "0.8", "0.9", "1.0", "1.1", "1.2", "1.4", "1.6",
-      "1.8", "2", "2.2", "2.5", "2.8", "3.2", "3.5", "4",
-      "4.5", "5", "5.6", "6.3", "7.1", "8", "9", "10", "11",
-      "13", "14", "16", "18", "20", "22"
-    ];
-
-    var form_divs = $(".aperture-slider")
-
-    for(var i = 0; i<form_divs.length; i++){
-      var id = $(form_divs[i]).attr('fid');
-      var maxAp = $(form_divs[i]).attr('maxAp');
-      var apUsed = $(form_divs[i]).attr('apUsed');
-      var steps_as_floats = string_to_float(steps);
-      $("#"+id+"aperture-slider").slider({
-        value: find_nearest(steps_as_floats, parseFloat(apUsed)),
-        min: find_nearest(steps_as_floats, parseFloat(maxAp)),
-        max: 30,
-        step: 1,
-        slide: function(event, ui) {
-          //console.log("I am:"+$(this).attr("fid")); 
-          $("#"+$(this).attr("fid")+"aperture-hint").html(steps[ui.value])
-        }
-      }); 
-      
-      $("#"+id+"aperture-hint").html(steps[$("#"+id+"aperture-slider").slider("value")]);
-    };
-
-  });
-
-  function string_to_float (array) {
-    holder = [];
-    for (var i = 0; i < array.length; i++){
-      holder[i] = parseFloat(array[i]);
-    }
-    return holder;
-  }
-
-  function find_nearest (array, val) {
-    var nearest = -1
-    var bestDist = 1000
-    var d = 1000
-    var index = 0
-    for (var i = 0; i < array.length; i++){
-      if (array[i] === val) {
-        return i;}
-      else {
-        d = Math.abs(val - array[i]);
-        if (d < bestDist) {
-          nearest = array[i]
-          var index = i;
-          bestDist = d;
-        }
-      }
-    }
-    return index;
-  };*/
 
