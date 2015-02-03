7fcfb9a632cfc8f3a86cbbea0cf581ffb1e67cef
Crystal Perreira <cvperreira@gmail.com>
2015-02-03 10:10:28 -0800

    update find_nearest to return index

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -17,44 +17,54 @@ $(document).ready(function () {
     var form_divs = $(".test-slider")
 
     for(var i = 0; i<form_divs.length; i++){
-      var id = $(form_divs[i]).attr('fid')
-      //var hint_thing = $("#"+$(".test-slider").id+"-hint")
-      //console.log(id)
-      console.log(id)
+      var id = $(form_divs[i]).attr('fid');
+      var maxAp = $(form_divs[i]).attr('maxAp');
+      console.log("Maxap as string:"+maxAp+"as float:"+parseFloat(maxAp));
+      console.log("array of numbers:"+string_to_float(steps));
+      console.log("nearest:"+find_nearest(string_to_float(steps), 4));
+      console.log("The maxAp is:" + maxAp + "and the closest thing is" + steps[find_nearest(string_to_float(steps), parseFloat(maxAp))]);
       $("#"+id+"-slider").slider({
         value: 1,
-        min: 0,
-        max: 4,
+        min: find_nearest(string_to_float(steps), parseFloat(maxAp)),
+        max: 30,
         step: 1,
         slide: function(event, ui) {
-        console.log("I am:"+$(this).attr("fid")); 
-          $("#"+$(this).attr("fid")+"-hint").html(stuff[ui.value]);
+          //console.log("I am:"+$(this).attr("fid")); 
+          $("#"+$(this).attr("fid")+"-hint").html(steps[ui.value])
         }
       }); 
       
       $("#"+id+"-hint").html(stuff[$("#"+id+"-slider").slider("value")]);
     };
 
-    //$(".big-hint").each.val(stuff[$(this).prev().slider("value")]);
   });
 
+  function string_to_float (array) {
+    holder = [];
+    for (var i = 0; i < array.length; i++){
+      holder[i] = parseFloat(array[i]);
+    }
+    return holder;
+  }
 
   function find_nearest (array, val) {
     var nearest = -1
     var bestDist = 1000
     var d = 1000
+    var index = 0
     for (var i = 0; i < array.length; i++){
       if (array[i] === val) {
-        return array[i];}
+        return i;}
       else {
         d = Math.abs(val - array[i]);
         if (d < bestDist) {
           nearest = array[i]
-          bestDist = d
+          var index = i;
+          bestDist = d;
         }
       }
     }
-    return nearest;
+    return index;
   };
 
 });
