973e4dc808ad94e6d73e574cc1af51a9ebadd6ae
Crystal Perreira <cvperreira@gmail.com>
2015-02-04 17:42:54 -0800

    can't submit multiple critiques

--- app/controllers/critiques_controller.rb
+++ app/controllers/critiques_controller.rb
@@ -1,16 +1,18 @@
 class CritiquesController < ApplicationController
   
   def submit_critique
-    @critique = Critique.create(
-      fid: params[:fid],
-      author: params[:author],
-      sugg_ap: params[:sugg_ap],
-      sugg_sh: params[:sugg_sh],
-      sugg_iso: params[:sugg_iso]
-    ) 
-    if params[:sugg_wb]
-      @critique.sugg_wb = params[:sugg_wb]
-      @critique.save
+    if Critique.where(fid: params[:fid], author: params[:author]).size < 1
+      @critique = Critique.create(
+        fid: params[:fid],
+        author: params[:author],
+        sugg_ap: params[:sugg_ap],
+        sugg_sh: params[:sugg_sh],
+        sugg_iso: params[:sugg_iso]
+      ) 
+      if params[:sugg_wb]
+        @critique.sugg_wb = params[:sugg_wb]
+        @critique.save
+      end
     end
     render :nothing => true
   end
