3145e6dcf191012991cecd9af04dfd5d26baf680
Crystal Perreira <cvperreira@gmail.com>
2015-02-09 13:46:11 -0800

    minor photo caveat cleanup

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ActiveRecord::Base
   validates :fid, uniqueness: true
-  validates :max_aperture_value, :lens, :model, :exposure_time, :iso, :white_balance, :f_number, :flash, :ev,  presence: true
+  validates :medium_640, :max_aperture_value, :lens, :model, :exposure_time, :iso, :white_balance, :f_number, :flash, :ev,  presence: true
   belongs_to :user
   has_many :critiques
 end
