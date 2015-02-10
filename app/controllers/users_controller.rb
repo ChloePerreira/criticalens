4d98a62849efc403659074cc106a7ffd86fe2b8c
Crystal Perreira <cvperreira@gmail.com>
2015-02-09 18:06:58 -0800

    fix small bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     if params[:show_diff_data] == "all"
-      @photos = Photo.all
+      @photos = @user.photos
     elsif params[:show_diff_data] == "no critiques"
       @photos = get_photos_wo_critiques(params[:id])
     else
       @photos = []
     end
   end
-
 
 end
