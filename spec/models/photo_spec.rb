9cf9a84605ec97332e148cbf2a8daea15824223b
Crystal Perreira <cvperreira@gmail.com>
2015-02-18 13:56:32 -0800

    start photo spec

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -9,7 +9,11 @@ describe ".validates" do
     expect(Photo.new(max_aperture_value: nil)).to_not be_valid
   end
 
-  it "must have a max_aperture_value field" do
-    expect(Photo.new(max_aperture_value: nil)).to_not be_valid
+  it "must have a lens field" do
+    expect(Photo.new(lens: nil)).to_not be_valid
+  end
+
+  it "must have a model field" do
+    expect(Photo.new(model: nil)).to_not be_valid
   end
 end
