1d185c90526c87d698734db1a0a1ad8caa234e79
Crystal Perreira <cvperreira@gmail.com>
2015-02-20 11:15:14 -0800

    add critiques recd method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   validates :name, presence: :true
   #validates_format_of :email, :with => /@/
   has_many :photos
+
+  def critiques_received
+    qty = 0
+    self.photos.each do |photo|
+      qty += photo.get_critiques.length
+    end
+    return qty
+  end
+
 end
